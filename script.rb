8144a1eaf0b381e45d62d3f09b350b6a7db16bc6
lecontroller <anujgupta@windowslive.com>
2022-06-05 12:53:44 -0400

    added victory conditions

--- script.rb
+++ script.rb
@@ -20,52 +20,83 @@ class TicTacToe
     return @board
   end
 
+  attr_reader :board
+
   def get_board
     puts "CURRENT BOARD"
     puts @board
     puts "\n"
   end
 
+  def get_board_position(number)
+    puts @board[number]
+  end
+
   def play_turn(player_symbol, grid_number)
     @board.gsub!(grid_number, player_symbol)
   end
 
-  def check_victory(board)
-    if true
-    end 
-  end
 end
 
+def check_victory(player_symbol, board)
+  if (board[0] == player_symbol && board[2] == player_symbol && board[4] == player_symbol) ||
+     (board[6] == player_symbol && board[8] == player_symbol && board[10] == player_symbol) ||
+     (board[12] == player_symbol && board[14] == player_symbol && board[16] == player_symbol) ||
+     (board[0] == player_symbol && board[6] == player_symbol && board[12] == player_symbol) ||
+     (board[2] == player_symbol && board[8] == player_symbol && board[14] == player_symbol) ||
+     (board[4] == player_symbol && board[10] == player_symbol && board[16] == player_symbol) ||
+     (board[0] == player_symbol && board[8] == player_symbol && board[16] == player_symbol) ||
+     (board[4] == player_symbol && board[8] == player_symbol && board[12] == player_symbol)
+    puts "VICTORY for #{player_symbol}!"
+    return true
+  end 
+end
 
 game = TicTacToe.new()
-
 game.pick_symbols()
-
 puts game.create_board
 
 victory = false
 while victory == false do
-  while true do
-    puts "PLAYER 1 (#{game.player1Symbol}): PICK A GRID SQUARE (1-9)"
-    input = gets.chomp.to_i
 
-    if input >= 1 && input <= 9
-      game.play_turn(game.player1Symbol, input.to_s)
-      break
+  if !check_victory(game.player2Symbol, game.board)
+
+    while true do
+      puts "PLAYER 1 (#{game.player1Symbol}): PICK A GRID SQUARE (1-9)"
+      input = gets.chomp.to_i
+
+      if input >= 1 && input <= 9
+        game.play_turn(game.player1Symbol, input.to_s)
+        break
+      end
     end
+  
+  else
+    victory = true
+    break
+  end
+  
+  game.get_board
+  
+
+  if !check_victory(game.player1Symbol, game.board)
+
+    while true do
+      puts "PLAYER 2 (#{game.player2Symbol}): PICK A GRID SQUARE (1-9)"
+      input = gets.chomp.to_i
+
+      if input >= 1 && input <= 9
+        game.play_turn(game.player2Symbol, input.to_s)
+        break
+      end
+    end
+
+  else
+    victory = true
+    break
   end
 
   game.get_board
+  
 
-  while true do
-    puts "PLAYER 2 (#{game.player2Symbol}): PICK A GRID SQUARE (1-9)"
-    input = gets.chomp.to_i
-
-    if input >= 1 && input <= 9
-      game.play_turn(game.player2Symbol, input.to_s)
-      break
-    end
-  end
-
-  game.get_board
 end
